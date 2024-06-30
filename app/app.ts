913076012342c9817aeb6c627064a35f9b7a7a9d
vamsimadhav <vamsimadhav2000@gmail.com>
2024-06-30 16:15:55 +0530

    only trials facing issues

--- app/app.ts
+++ app/app.ts
@@ -4,8 +4,32 @@ You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first module.
 */
 
-import { Application } from '@nativescript/core'
+import { Application, AndroidApplication, Utils } from '@nativescript/core';
+import { NSBranchModule } from 'native-script-branch'
 
+// Ensure your app is set up correctly
+Application.on(Application.launchEvent, () => {
+    const context = Utils.android.getApplicationContext(); // Get Android application context
+
+    NSBranchModule.getAutoInstance(context)
+        .then(() => {
+            console.log('Branch autoInstance successfully initialized');
+        })
+        .catch((error) => {
+            console.error('Branch autoInstance initialization failed:', error);
+        });
+});
+// Application.android.on(Application.android.activityStartedEvent, (args: any) => {
+//     const activity = args.activity;
+//     console.log("Activity started, initializing Branch session");
+//     NSBranchModule.sessionBuilder(activity)
+//         .then(() => {
+//             console.log("Branch session initialized successfully");
+//         })
+//         .catch((error) => {
+//             console.error("Branch session initialization failed:", error);
+//         });
+// });
 Application.run({ moduleName: 'app-root' })
 
 /*
--- app/main-page.ts
+++ app/main-page.ts
@@ -1,5 +1,7 @@
 import { EventData, Page } from '@nativescript/core'
 import { HelloWorldModel } from './main-view-model'
+import { Application } from '@nativescript/core';
+import { NSBranchModule } from 'native-script-branch';
 
 export function navigatingTo(args: EventData) {
   const page = <Page>args.object
